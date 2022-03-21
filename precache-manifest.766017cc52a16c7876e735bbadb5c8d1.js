40a8fc16f81eebde6fc4aa3b1a1bb89d1cecd2a8
MadakaHeri <MadakaHeri@users.noreply.github.com>
2022-03-21 18:52:13 +0000

    deploy: f098caae99784d514be058031fa405b0a2510a9e

---
+++ precache-manifest.766017cc52a16c7876e735bbadb5c8d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7b912fe6d78c384a340d17ea715579",
+    "url": "/.well-known/brave-rewards-verification.txt"
+  },
+  {
+    "revision": "24158f25644a0ee44115",
+    "url": "/css/chunk-vendors.87a8c1c5.css"
+  },
+  {
+    "revision": "f77ff83cd22c0b10de7b",
+    "url": "/css/index.44a60de4.css"
+  },
+  {
+    "revision": "ef585b7c576c4763bcd8ebeab30757a4",
+    "url": "/img/avator.ef585b7c.png"
+  },
+  {
+    "revision": "363e935ee79a263b39bfac0876dc4cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24158f25644a0ee44115",
+    "url": "/js/chunk-vendors.e67bc27a.js"
+  },
+  {
+    "revision": "b67b74e314adb5656666",
+    "url": "/js/credits.ef241e88.js"
+  },
+  {
+    "revision": "f287cc1fa6b37272358c",
+    "url": "/js/home.c0538473.js"
+  },
+  {
+    "revision": "f77ff83cd22c0b10de7b",
+    "url": "/js/index.e3701df5.js"
+  },
+  {
+    "revision": "fac33212697fa4a1d795",
+    "url": "/js/profile.8155c032.js"
+  },
+  {
+    "revision": "f5fb4b141c929d4ccbdc",
+    "url": "/js/profile~sns.b0ae947a.js"
+  },
+  {
+    "revision": "5e26cbd1cda8ec71183c",
+    "url": "/js/sns.c7a6d8e7.js"
+  },
+  {
+    "revision": "767c1c549df05932e3de8012989d529a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
